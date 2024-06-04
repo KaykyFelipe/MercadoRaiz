10cd1f51773ab1a755a37c9718465a8233e5b6b8
Kayky Felipe do Nascimento <kayky159@gmail.com>
2024-06-04 11:41:50 -0300

    Commit 1

---
+++ Projeto PIM/PIM III/PIM III/Usuario.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIM_III
+{
+    internal class Usuario
+    {
+
+        public string nome = null;
+        private string cpf;
+        private string telefone;
+        private string endereço;
+        private string cep;
+        private string email;
+        private string senha;
+
+
+
+
+        //Apresentação do Programa
+
+
+
+        public void Cadastro() {
+
+           Console.WriteLine("Digite seu Nome: \n");
+           nome = Console.ReadLine(); //GUARDAR NO BANCO DE DADOS
+
+           Console.WriteLine("Digite seu Telefone: \n");
+
+           Console.WriteLine("Digite seu CEP: \n");
+
+           Console.WriteLine("Digite seu Endereço: \n");
+
+           Console.WriteLine("Digite seu E-mail: \n");
+
+           Console.WriteLine("Crie uma Senha Numerica de 4 digitos: \n");
+
+        }
+
+    }
+}
